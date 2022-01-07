6a4466bacd1f6f9862c2202e3c9a67dcadbfcddd
taro-hida <sk8trou@gmail.com>
2022-01-07 21:19:08 +0900

    esa624 不正な URL が渡された場合に例外を投げるように変更

--- src/Domain/DiscordImage/DiscordImageUrl.php
+++ src/Domain/DiscordImage/DiscordImageUrl.php
@@ -3,17 +3,20 @@ declare(strict_types=1);
 
 namespace App\Domain\DiscordImage;
 
+use App\Domain\DiscordImage\Exception\UrlValidateException;
 use App\Infrastructure\ImageDownloadClient\ImageDownloadClientInterface;
 
 class DiscordImageUrl
 {
     private string $url;
 
+    /**
+     * @throws UrlValidateException
+     */
     public function __construct(string $url)
     {
         if (!filter_var($url, FILTER_VALIDATE_URL)) {
-            // todo: 例外を定義する
-            throw new \RuntimeException();
+            throw new UrlValidateException();
         }
         $this->url = $url;
     }
