9815a675ea680935538e9810409739ba6a69bf91
taro-hida <sk8trou@gmail.com>
2022-01-11 09:28:50 +0900

    esa624 $expected_image という変数名が不適切となっていたため修正

--- tests/Domain/DiscordImage/DiscordImageUrlTest.php
+++ tests/Domain/DiscordImage/DiscordImageUrlTest.php
@@ -17,9 +17,9 @@ class DiscordImageUrlTest extends AppTestCase
     public function test_method_fetchImage_with_mock_call_expected_arg_and_return_image()
     {
         $url = 'https://example.example';
-        $expected_image = 'this is image byte string';
-        $client_mock = $this->createDownloadClientMockExpectCallMethodFetchImageWithUrlAndReturnImage($url, $expected_image);
+        $image_byte_string = 'this is image byte string';
+        $client_mock = $this->createDownloadClientMockExpectCallMethodFetchImageWithUrlAndReturnImage($url, $image_byte_string);
         $image = new DiscordImageUrl($url);
-        self::assertObjectEquals(new DiscordImage($expected_image), $image->fetchImage($client_mock));
+        self::assertObjectEquals(new DiscordImage($image_byte_string), $image->fetchImage($client_mock));
     }
 }
